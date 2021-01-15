05c858babd1e6a902e09c9799182a2560b2e0ebf
PilYeong <pilyeooong@gmail.com>
2021-01-15 22:59:54 +0900

    ADD: seperate common configuration for testing

---
+++ backend/src/test/config.ts
@@ -0,0 +1,21 @@
+import { ConnectionOptions } from "typeorm";
+import entities from '../entity';
+
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const ConnectionOptions: ConnectionOptions = {
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT!,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_TEST_DATABASE,
+  dropSchema: true,
+  synchronize: true,
+  logging: false,
+  entities,
+};
+
+export default ConnectionOptions;
