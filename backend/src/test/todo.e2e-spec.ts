84011df31a505a04f24f900e1676fd6dd77df77a
PilYeong <pilyeooong@gmail.com>
2021-01-15 23:00:13 +0900

    ADD: E2E testing

---
+++ backend/src/test/todo.e2e-spec.ts
@@ -0,0 +1,139 @@
+import * as request from 'supertest';
+import { Connection, createConnection, getConnection } from 'typeorm';
+import * as dotenv from 'dotenv';
+import app from '../app';
+import ConnectionOptions from './config';
+
+dotenv.config();
+
+const TEST_EMAIL = 'test@test.com';
+const TEST_ANOTHER_EMAIL = 'testAnother@test.com';
+const TEST_PASSWORD = 'testPassword';
+const TEST_CONTENT = 'testContent';
+
+let token: string;
+let secondToken: string;
+
+beforeAll(async () => {
+  await createConnection(ConnectionOptions);
+});
+
+const agent = request.agent(app);
+
+describe('토큰 발급', () => {
+  it('유효한 정보가 전달되면 회원가입 수행 및 200 응답코드를 반환', async (done) => {
+    const res = await request(app).post('/api/user/').send({
+      email: TEST_EMAIL,
+      password: TEST_PASSWORD,
+    });
+    expect(res.status).toEqual(201);
+    done();
+  });
+
+  it('유효한 정보가 전달되면 두번째 계정에 대한 회원가입 수행 및 200 응답코드를 반환', async (done) => {
+    const res = await request(app).post('/api/user/').send({
+      email: TEST_ANOTHER_EMAIL,
+      password: TEST_PASSWORD,
+    });
+    expect(res.status).toEqual(201);
+    done();
+  });
+
+  it('로그인 및 토큰 발급', async (done) => {
+    const res = await request(app).post('/api/user/login').send({
+      email: TEST_EMAIL,
+      password: TEST_PASSWORD,
+    });
+    expect(res.status).toEqual(200);
+    token = res.body.token;
+    agent.set('Authorization', token);
+    done();
+  });
+
+  it('두번째 계정에 로그인 및 토큰 발급', async (done) => {
+    const res = await request(app).post('/api/user/login').send({
+      email: TEST_ANOTHER_EMAIL,
+      password: TEST_PASSWORD,
+    });
+    expect(res.status).toEqual(200);
+    secondToken = res.body.token;
+    done();
+  });
+});
+
+describe('POST /', () => {
+  it('req.body에 content가 비어있을 시 400 에러코드를 반환', async (done) => {
+    const res = await agent.post('/api/todo');
+    expect(res.status).toEqual(400);
+    expect(res.text).toEqual('빈 내용은 추가 할 수 없습니다.');
+    done();
+  });
+
+  it('유효한 요청일 시 schedule에 todo 추가 및 200 응답코드를 반환', async (done) => {
+    const res = await agent.post('/api/todo').send({
+      content: TEST_CONTENT,
+    });
+    expect(res.status).toEqual(201);
+    done();
+  });
+});
+
+describe('GET /:todoId', () => {
+  it('본인 schedule에 속한 todo가 아닌 것을 요청할 시 400 에러코드를 반환', async (done) => {
+    const res = await request(app)
+      .get('/api/todo/1')
+      .set('Authorization', secondToken);
+    expect(res.status).toEqual(400);
+    expect(res.text).toEqual('존재하지 않는 todo 입니다.');
+    done();
+  });
+
+  it('유효한 요청일 시 todo 상세정보와 200 응답코드를 반환', async (done) => {
+    const res = await agent.get('/api/todo/1');
+    expect(res.status).toEqual(200);
+    done();
+  });
+});
+
+describe('PATCH /:todoId', () => {
+  it('본인 schedule에 속한 todo가 아닌 것에 대해 수정을 시도 할 시 400 에러코드를 반환', async (done) => {
+    const res = await request(app)
+      .patch('/api/todo/1')
+      .set('Authorization', secondToken);
+    expect(res.status).toEqual(400);
+    expect(res.text).toEqual('존재하지 않는 todo 입니다.');
+    done();
+  });
+
+  it('유효한 요청일 시 todo를 수정, 200 응답코드를 반환', async (done) => {
+    const res = await agent.patch('/api/todo/1').send({
+      content: `I'm trying to edit`,
+    });
+    expect(res.status).toEqual(200);
+    expect(res.text).toEqual('수정 완료');
+    done();
+  });
+});
+
+describe('DELETE /:todoId', () => {
+  it('본인 schedule에 속한 todo가 아닌 것에 대해 삭제를 시도 할 시 400 에러코드를 반환', async (done) => {
+    const res = await request(app)
+      .delete('/api/todo/1')
+      .set('Authorization', secondToken);
+    expect(res.status).toEqual(400);
+    expect(res.text).toEqual('존재하지 않는 todo 입니다.');
+    done();
+  });
+
+  it('유효한 요청일 시 todo를 삭제, 200 응답코드를 반환', async (done) => {
+    const res = await agent.delete('/api/todo/1');
+    expect(res.status).toEqual(200);
+    expect(res.text).toEqual('삭제 완료');
+    done();
+  });
+});
+
+afterAll(async () => {
+  const connection: Connection = getConnection();
+  await connection.close();
+});
--- backend/src/test/user.e2e-spec.ts
+++ backend/src/test/user.e2e-spec.ts
@@ -3,27 +3,11 @@ import app from '../app';
 import * as dotenv from 'dotenv';
 import {
   Connection,
-  ConnectionOptions,
   createConnection,
   getConnection,
 } from 'typeorm';
-import entities from '../entity';
-
-dotenv.config();
-
-const ConnectionOptions: ConnectionOptions = {
-  type: 'mysql',
-  host: process.env.DB_HOST,
-  port: +process.env.DB_PORT!,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_TEST_DATABASE,
-  dropSchema: true,
-  synchronize: true,
-  logging: false,
-  entities,
-};
-
+import ConnectionOptions from './config';
+ 
 beforeAll(async () => {
   await createConnection(ConnectionOptions);
 });
@@ -55,27 +39,59 @@ describe('POST /', () => {
       email: TEST_EMAIL,
       password: TEST_PASSWORD,
     });
-    expect(res.status).toEqual(200);
+    expect(res.status).toEqual(201);
+    done();
+  });
+
+  it('이미 존재하는 이메일로 회원 가입 시도시 400 에러코드를 반환', async (done) => {
+    const res = await request(app).post('/api/user/').send({
+      email: TEST_EMAIL,
+      password: TEST_PASSWORD,
+    });
+    expect(res.status).toEqual(400);
+    expect(res.text).toEqual('해당 이메일로 가입 된 계정이 존재합니다.');
     done();
   });
 });
 
 describe('POST /login', () => {
-  it('로그인 성공 시 200 응답코드와 토큰을 발급받는다', async () => {
+  it('존재하지 않는 이메일로 로그인 시도 시 400 에러를 반환', async (done) => {
+    const res = await request(app).post('/api/user/login').send({
+      email: 'notExist@gmail.com',
+      password: TEST_PASSWORD,
+    });
+    expect(res.status).toEqual(400);
+    expect(res.text).toEqual('존재하지 않는 사용자입니다.');
+    done();
+  });
+
+  it('잘못된 비밀번호로 로그인 시 400 에러를 반환', async (done) => {
+    const res = await request(app).post('/api/user/login').send({
+      email: TEST_EMAIL,
+      password: 'wrongPassword'
+    });
+    expect(res.status).toEqual(400);
+    expect(res.text).toEqual('비밀번호가 일치하지 않습니다.');
+    done();
+  });
+
+  it('로그인 성공 시 200 응답코드와 토큰을 발급받는다', async (done) => {
     const res = await request(app).post('/api/user/login').send({
       email: TEST_EMAIL,
       password: TEST_PASSWORD,
     });
     token = res.body.token;
     expect(res.status).toEqual(200);
+    done();
   });
 });
 
 describe('GET /', () => {
-  it('올바른 토큰일 시 내 정보와 200 응답코드를 반환', async () => {
+  it('올바른 토큰일 시 내 정보와 200 응답코드를 반환', async (done) => {
     const res = await request(app).get('/api/user').set('Authorization', token);
     expect(res.status).toEqual(200);
     expect(res.body.email).toEqual(TEST_EMAIL);
+    done();
   });
 });
 
