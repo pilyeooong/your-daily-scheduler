0af96d181bbd164e4c7177b970dbbc3f3d1fb02f
PilYeong <pilyeooong@gmail.com>
2021-02-23 17:16:50 +0900

    CHG: port

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,6 +4,6 @@ const prod = process.env.NODE_ENV === 'production';
 
 const PORT = prod ? 80 : 4000;
 
-app.listen(PORT, () => {
+app.listen(4000, () => {
   console.log(`server is running on port ${PORT}`);
 });
