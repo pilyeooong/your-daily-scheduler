fa881d0e502f0a39eab80ae4aa3804d72ea09cf8
PilYeong <pilyeooong@gmail.com>
2021-02-26 17:48:32 +0900

    ADD: meta tags

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -8,6 +8,7 @@ import LoggedInRouter from '../routers/Logged-in-router';
 import LoggedOutRouter from '../routers/Logged-out-router';
 
 import 'react-toastify/dist/ReactToastify.min.css';
+import { Helmet } from 'react-helmet';
 
 function App() {
   const dispatch = useDispatch();
@@ -19,6 +20,11 @@ function App() {
 
   return (
     <>
+      <Helmet>
+        <title>YOUR DAILY SCHEDULER</title>
+        <meta property="og:title" content={'YOUR DAILY SCHEDULER'} />
+        <meta name="description" content={'하루 일정을 기록해 보세요 !'} />
+      </Helmet>
       {me ? <LoggedInRouter /> : <LoggedOutRouter />}
       <ToastContainer />
     </>
