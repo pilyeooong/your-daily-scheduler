c6b91a68299a0e7a07b456845954755befdc231c
PilYeong <pilyeooong@gmail.com>
2021-04-23 14:13:39 +0900

    FIX: PORT

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,6 @@
 import app from './app';
 
-const prod = process.env.NODE_ENV === 'production';
-
-const PORT = prod ? 80 : 4000;
+const PORT = 4000;
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
